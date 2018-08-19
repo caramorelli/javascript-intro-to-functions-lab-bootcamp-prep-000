177f1135b4f249221e368c52693cfb098d343984
Cara Morelli <caramorelli7@gmail.com>
2018-08-19 20:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ function whisper(string) {
 }
 
 function logShout(string) {
-   console.log(shout(string))
+   console.log(shout(string));
 }
 
 function logWhisper(string) {
-   console.log(whisper(string))
+   console.log(whisper(string));
 }
 
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
-     return 'I love you, too.'
+     return 'I love you, too.';
   }
   else if (string === whisper(string)) {
-     return 'I can\'t hear you!'
+     return 'I can\'t hear you!';
   }
   else if (string === shout(string)) {
-     return 'YES INDEED!'
+     return 'YES INDEED!';
   }
 }
 
